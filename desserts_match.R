8b9d62e8ecc2d35e1135e62159edbcfcfe65617e
emmabeamitchell <ebeatrice19@gmail.com>
2024-08-26 15:59:04 -0700

    fixing

--- desserts_match.R
+++ desserts_match.R
@@ -1,14 +1,13 @@
 
-d
+
 
 same <- dessert_listing %>%
   if(str_detect("Brownie" | "Ice cream cake"))
     print("it's a match")
 
-<<<<<<< HEAD
-i am making a different changedFiles()
-=======
+#i am making a different changedFiles
+
 if(str_detect("Brownie" | "Ice cream cake")) {
 
 }
->>>>>>> 92510708e1dad20b6794079174b23072d2cd8c7b
+
