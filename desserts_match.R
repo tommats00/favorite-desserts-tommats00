131ed2d55cb371054d6629e5a2238aa77395d678
emmabeamitchell <ebeatrice19@gmail.com>
2024-08-26 15:46:02 -0700

    changes

--- desserts_match.R
+++ desserts_match.R
@@ -20,3 +20,9 @@ dessert_listing <- dessert_elements %>%
   head(.,-3) %>%               # 3 last ones were not desserts
   rowid_to_column("rank") %>%  # adding a column using the row number as a proxy for the rank
   write_csv("data/iconic_desserts.csv") # save it as csv
+
+
+
+
+
+
