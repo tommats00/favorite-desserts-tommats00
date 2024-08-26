92510708e1dad20b6794079174b23072d2cd8c7b
tommats00 <tbg@bren.ucsb.edu>
2024-08-26 15:48:40 -0700

    change

--- desserts_match.R
+++ desserts_match.R
@@ -1,27 +1,10 @@
 
-library(here)
-library(readr)
-favorite_dessert <- read_csv(here("favorite_desserts.csv"))
-library(tidyverse)
-library(rvest)  # used to scrape website content
-
-# Check if that data folder exists and creates it if not
-dir.create("data", showWarnings = FALSE)
-
-# Read the webpage code
-webpage <- read_html("https://www.eatthis.com/iconic-desserts-united-states/")
-
-# Extract the desserts listing
-dessert_elements<- html_elements(webpage, "h2")
-dessert_listing <- dessert_elements %>%
-  html_text2() %>%             # extracting the text associated with this type of element of the webpage
-  as_tibble() %>%              # make it a data frame
-  rename(dessert = value) %>%  # better name for the column
-  head(.,-3) %>%               # 3 last ones were not desserts
-  rowid_to_column("rank") %>%  # adding a column using the row number as a proxy for the rank
-  write_csv("data/iconic_desserts.csv") # save it as csv
+d
 
 same <- dessert_listing %>%
   if(str_detect("Brownie" | "Ice cream cake"))
     print("it's a match")
 
+if(str_detect("Brownie" | "Ice cream cake")) {
+
+}
